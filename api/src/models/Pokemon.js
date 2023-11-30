f3c3db74385348af5244b3ca5222dfaa3997665c
sthefanymendez <sthefanymendez.12@gmail.com>
2023-11-30 16:31:26 -0500

    progress

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    strength: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, { timestamps: false});
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
+// const { Pokemon } = require('../db')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.post('/pokemon', async (req, res) => { 
+    // Pokemon.create({ name: 'xbcnxc' })
+    // await fetch('https://pokeapi.co/api/v2/pokemon')
+    //     .then(response => response.json())
+    //     .then(data => res.json(data))
+    const { id, name, hp, strength, defense, speed, height, weight } = req.body;
+    if (!id || !name) {
+        res.json({ error: 'this field is required' })
+    }
+    // console.log(defense)
+
+    res.json({ message: 'correct action'})
+    
+});
 
 
 module.exports = router;
