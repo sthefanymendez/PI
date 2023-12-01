8fc2b6e748ce376f0fa27fb63b90317499369147
sthefanymendez <sthefanymendez.12@gmail.com>
2023-12-01 13:03:06 -0500

    progress

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,25 +1,37 @@
 const { Router } = require('express');
-// const { Pokemon } = require('../db')
+const { Pokemon, Type } = require('../db')
 // Importar todos los routers;
+// const Pokemon = require('../models/Pokemon.js')
+// const Type = require('../models/Type.js')
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.post('/pokemon', async (req, res) => { 
+// router.get('/pokemon', async (req, res) => {
+
+// })
+router.post('/pokemon', async (req, res) => {
     // Pokemon.create({ name: 'xbcnxc' })
     // await fetch('https://pokeapi.co/api/v2/pokemon')
     //     .then(response => response.json())
     //     .then(data => res.json(data))
-    const { id, name, hp, strength, defense, speed, height, weight } = req.body;
-    if (!id || !name) {
-        res.json({ error: 'this field is required' })
-    }
-    // console.log(defense)
 
-    res.json({ message: 'correct action'})
+    const { name, hp, strength, defense, speed, height, weight } = req.body;
+
+    if (!name) return res.status(422).send('this field is required');
+
+    const pokemon = await Pokemon.create({ name, hp, strength, defense, speed, height, weight });
+
+    const type = await Type.findAll({
+        where: { name: { [Op.in]: types } }
+    });
     
-});
+    pokemon.addType(type);
 
+    res.status(200).json({ message: 'correct action' });
+    
+    // console.log(defense)
+});
 
 module.exports = router;
