0a9488494e1cb1fb8f345bdd5a184f5459841e04
sthefanymendez <sthefanymendez.12@gmail.com>
2024-02-05 13:17:59 -0500

    progress

---
+++ client/src/components/Filters.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTypes } from "../redux/actions";
+
+const Filters = () => {
+    const dispatch = useDispatch()
+    const types = useSelector(state => state.types)
+
+    useEffect(() => {
+        dispatch(getTypes())
+    }, [dispatch])
+
+    return (
+        <div>
+            <div>
+                <h3>Pokemon types</h3>
+                <ul>
+                    {
+                        types?.map((type, index) => {
+                            return (
+                                <li key={index}>
+                                    {type.name}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+            <div></div>
+            {/* <div>
+                <h3>Filters</h3>
+                <div>
+                    <div>
+                        <div>Types</div>
+                        <ul>
+                            {
+                                types?.map((type, index) => {
+                                    return (
+                                        <li key={index}>
+                                            {type.name}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div>Existings</div>
+                    <label class=''>
+                        <input type="text" />
+                        <span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </label>
+                    <div>Aggregates</div>
+                    <label class='checkbox'>
+                        <input type="text" />
+                        <span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </label>
+                </div>
+            </div> */}
+        </div>
+    )
+}
+
+export default Filters
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,21 +5,31 @@ import Cards from "./cards/Cards";
 import Search from "./Search";
 import Pager from "./Pager";
 import Orders from "./Orders";
+import Filters from "./Filters";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom";
 
 const Home = () => {
-  return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100%", justifyContent: "space-around" }}>
-      <Search />
-      <Link to='/add'>
-        <button>
-          Add Pokemon
-        </button>
-      </Link>
-      <Orders />
-      <Cards />
-      <Pager />
-    </div>
-  );
+    const location = useLocation()
+    const history = useHistory()
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100%", justifyContent: "space-around" }}>
+            <button onClick={() => history.push('&hola=hola')}>agregar</button>
+            <button onClick={() => console.log({ location, history })}>agregar</button>
+            <Search />
+            <Link to='/add'>
+                <button>
+                    Add Pokemon
+                </button>
+            </Link>
+            <Orders />
+            <div style={{ display: "flex" }}>
+                <Filters />
+                <Cards />
+            </div>
+            <Pager />
+        </div>
+    );
 };
 
 export default Home
